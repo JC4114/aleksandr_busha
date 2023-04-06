080d39f645f7f8b22cdeb5c3fa0a9ded95cbadc7
JC4114 <4114114jc@gmail.com>
2023-04-06 18:02:24 +0300

    Chart

--- src/components/forma/PlayerForm.jsx
+++ src/components/forma/PlayerForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import Chart from './chart';
+import Chart from './Chart';
 import InputField from './InputField';
 import './PlayerForm.css';
 
